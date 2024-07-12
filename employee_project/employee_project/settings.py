92584a52b1db2a03f4862efe03f7be4606b40e19
HManthi <32368168+HManthi@users.noreply.github.com>
2024-07-12 14:55:02 +0100

    create database tables
    employee table and position table

--- employee_project/employee_project/settings.py
+++ employee_project/employee_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee_register'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,25 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#mysql database connection
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EmployeeDB',
+        'USER': 'root',  # your MySQL username
+        'PASSWORD': '',  # your MySQL password
+        'HOST': 'localhost',  # your MySQL host
+        'PORT': '3306',  # your MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
